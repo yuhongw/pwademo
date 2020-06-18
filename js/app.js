a8617e02a50fc3ec290381abbfb06a129de3b2fa
Bruce Yu <yuhon@live.com>
2020-06-18 16:10:11 +0800

    restore to original

--- js/app.js
+++ js/app.js
@@ -3,8 +3,7 @@ const coffees = [
   {
     name: "Perspiciatis",
     image: "images/coffee1.jpg"
-  }
-  ,
+  },
   {
     name: "Voluptatem",
     image: "images/coffee2.jpg"
@@ -58,7 +57,7 @@ document.addEventListener("DOMContentLoaded", showCoffees);
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("./serviceWorker.js")
+      .register("/serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });
